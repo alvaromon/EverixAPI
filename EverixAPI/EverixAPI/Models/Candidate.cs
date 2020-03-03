f209c2fce68dda56aae9529a97c42b4044f695e2
alvaromon <alvaro.montoya@knights.ucf.edu>
2020-03-02 23:06:16 -0500

    added API project and some preliminary db code
    using:
    
    dotnet ef dbcontext scaffold "Data Source=localhost;Initial Catalog=PrototypeTable;User ID=SA;Password=Oneshot1!" Microsoft.EntityFrameworkCore.SqlServer --project C:\Users\alvar\source\repos\EverixAPI\EverixAPI\EverixAPI\EverixAPI.csproj

---
+++ EverixAPI/EverixAPI/Models/Candidate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EverixAPI
+{
+    public partial class Candidate
+    {
+        public string Name { get; set; }
+        public string Party { get; set; }
+        public int? Funding { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ EverixAPI/EverixAPI/Models/PrototypeTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EverixAPI
+{
+    public partial class PrototypeTable
+    {
+        public int? PrototypeField { get; set; }
+        public int PrototypePk { get; set; }
+    }
+}
